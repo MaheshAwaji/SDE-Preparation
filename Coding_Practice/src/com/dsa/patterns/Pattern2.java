41dec63fc5296b53878e2a0a6c3e315aa0a1503f
MaheshAwaji <maheshawaji111@gmail.com>
2025-12-15 23:19:33 +0530

    Written Pattern codes

---
+++ Coding_Practice/src/com/dsa/patterns/Pattern2.java
@@ -0,0 +1,23 @@
+package com.dsa.patterns;
+
+public class Pattern2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		* * * * 
+//		* * * 
+//		* * 
+//		*
+		int n=5;
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<n;j++) {
+				if(j>i) {
+					System.out.print("* ");
+				}
+			}
+			System.out.println();
+		}
+
+	}
+
+}
